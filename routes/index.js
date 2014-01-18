8700f129f1c09b4f89f8713045094f6e9b2d0b01
Kevin <kev71187@gmail.com>
2014-01-17 22:13:50 -0800

    f

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ exports.index = function(req, res){
   var gpio = require("pi-gpio");
   gpio.open(7, "output", function(err) {
     gpio.write(7, 1, function() {   
+      setTimeout(2000, function() {
         gpio.close(7);
+      })
     });
   });
   res.render('index', { title: 'Express' });
